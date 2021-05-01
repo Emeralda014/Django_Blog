cdc1248bd509c64fc129c11802e6539001e62e72
Emeralda014 <emeraldcator@gmail.com>
2021-05-01 21:32:31 +0100

    Django -Blog

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Post(models.Model):
@@ -8,3 +9,16 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[str(self.id)])
+
+
+class Comment(models.Model):
+    post =  models.ForeignKey('Post', related_name = 'comments', on_delete=models.CASCADE,)
+    name = models.CharField(max_length=200)
+    body = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '%s - %s' % (self.post.title, self.name)
